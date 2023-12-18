0ca69a4c93179a4378148ed581c398b868fe70c0
Ollie Watt <ollie@tebex.co.uk>
2023-12-18 21:54:49 +0000

    PHPCS fix

--- src/PHPStan/ModelAttributeExtension.php
+++ src/PHPStan/ModelAttributeExtension.php
@@ -4,15 +4,17 @@ namespace LiamWiltshire\LaravelModelMeta\PHPStan;
 
 use Illuminate\Database\Eloquent\Model;
 use LiamWiltshire\LaravelModelMeta\Concerns\HasMeta;
+use PHPStan\Reflection\ClassReflection;
 use PHPStan\Reflection\PropertiesClassReflectionExtension;
+use PHPStan\Reflection\PropertyReflection;
 use Tebex\Checkout\Models\RecurringPayment;
 
 class ModelAttributeExtension implements PropertiesClassReflectionExtension
 {
-    public function hasProperty(\PHPStan\Reflection\ClassReflection $classReflection, string $propertyName): bool
+    public function hasProperty(ClassReflection $classReflection, string $propertyName): bool
     {
         foreach ($classReflection->getTraits() as $trait) {
-            if($trait->getName() === (string)HasMeta::class) {
+            if ($trait->getName() === (string)HasMeta::class) {
                 return true;
             }
         }
@@ -20,7 +22,7 @@ class ModelAttributeExtension implements PropertiesClassReflectionExtension
         return false;
     }
 
-    public function getProperty(\PHPStan\Reflection\ClassReflection $classReflection, string $propertyName): \PHPStan\Reflection\PropertyReflection
+    public function getProperty(ClassReflection $classReflection, string $propertyName): PropertyReflection
     {
         return new MetaProperty($classReflection, $propertyName);
     }
